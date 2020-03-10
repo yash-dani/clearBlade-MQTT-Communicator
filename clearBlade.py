c382698348799d64e83043aa472f9f8846adcf8f
yash-dani <daniyash19@gmail.com>
2020-03-09 21:41:42 -0400

    finished sending messages protocol

--- clearBlade.py
+++ clearBlade.py
@@ -8,21 +8,25 @@ import psutil
 from clearblade.ClearBladeCore import System, Query, Developer
 import time
 
-# Setup system using credentials
+# Setup system I created on the web console using credentials
 SystemKey = "8eedb6e60b8892f4e0e5d6fe8111"
 SystemSecret = "8EEDB6E60BFCD5B4E8A6F5E7E264"
-
 mySystem = System(SystemKey, SystemSecret)
 
 # Setup User
 yash = mySystem.User("yash.dani@uwaterloo.ca", "D00pamine!!")
 
 
+# Start messaging system
+mqtt = mySystem.Messaging(yash)
+
+# Connect
+mqtt.connect()
+
+# Test message 
+for i in range(20):
+	mqtt.publish("/test", psutil.cpu_percent())
+
+mqtt.disconnect()
 
 
-#get the CPU Usage using:
-'''
-while True:
-	print(psutil.cpu_percent())
-	time.sleep(1)
-'''
